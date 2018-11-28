6aa2d9c32afdf304df8a67397a87f85e7b38374c
tejaswini <tejaswini@LENOVO-PC>
2018-11-28 14:38:32 +0530

    1commit

---
+++ src/myFirstJava/Practice.java
@@ -0,0 +1,14 @@
+package myFirstJava;
+
+public class Practice {
+public static void main(String args[]) {
+	int i,j,k=1;
+	for (i=1; i<=10; i++) {
+		for(j=1; j<=i; j++) {
+			System.out.print(k+"\t");
+			k++;
+	}
+		System.out.println();
+}
+}
+}
---
+++ src/myFirstJava/Test.java
@@ -0,0 +1,9 @@
+package myFirstJava;
+
+class Test {
+      int a=10;
+      public static void main (String args[]) {
+    	  Test t = new Test();
+    	  System.out.println(t.a);
+      }
+}
